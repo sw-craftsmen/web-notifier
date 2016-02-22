c638dc58894471adcd10de3ce343eda4cf30e6e5
rkan <its_right@msn.com>
2016-02-22 18:12:02 +0800

    Use built-in module to retrieve web content

--- web_fetch/fetcher.py
+++ web_fetch/fetcher.py
@@ -6,7 +6,7 @@ import os
 import sys
 from subprocess import Popen, PIPE
 
-HTML_FILE_NAME = "wget.html"
+HTML_FILE_NAME = "retrieved_page.html"
 
 
 def get_web_content(url, login, password):
@@ -29,11 +29,41 @@ def locate_abs_exec(program):  # 'program' can be an absolute path name, or just
     return None
 
 
+# keep the 'wget' routine as somehow we may possibly still need it?
+def __get_html_by_wget(url, out_file, web_login,  web_password):
+    wget_abs_path = locate_abs_exec("wget")
+    if not wget_abs_path:
+        logging.error("wget is required but does not present, program exits")
+        sys.exit()
+    output, error = Popen([wget_abs_path, "-O", out_file, web_login, web_password, url],
+                          stdout=PIPE, stderr=PIPE).communicate()
+
+    logging.info(output + error)
+
+
+def __get_html_content(url, out_file, web_login,  web_password):
+    import urllib.request
+    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, url, web_login, web_password)
+    auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+    opener = urllib.request.build_opener(auth_handler)
+    urllib.request.install_opener(opener)
+
+    try:
+        web_content = urllib.request.urlopen(url)
+    except urllib.error.URLError as e:
+        logging.error(str(e))
+        return False
+
+    with open(out_file, 'wb') as write_fd:
+        write_fd.write(web_content.read().decode('big5', 'ignore').encode('utf-8'))
+    return True
+
+
 #
-# Call wget to retrieve web content 
+# Retrieve web content
 #
 def __get_html_lines(url, login, password, for_test):
-    html_lines = {}
  
     web_login = ''
     web_password = ''
@@ -44,15 +74,11 @@ def __get_html_lines(url, login, password, for_test):
 
     effective_url = url if not for_test else "http://www.grymoire.com/Unix/Quote.html"
 
-    wget_abs_path = locate_abs_exec("wget")
-    if not wget_abs_path:
-        logging.error("wget is required for web-notifier but does not present, program exits")
-        sys.exit()
-    output, error = Popen([wget_abs_path, "-O", HTML_FILE_NAME, web_login, web_password, effective_url],
-            stdout=PIPE, stderr=PIPE).communicate()
+    if not __get_html_content(effective_url, HTML_FILE_NAME, web_login, web_password):
+        logging.error("cannot open url: " + effective_url)
+        return {}
 
-    logging.info(output + error)
-
+    html_lines = {}
     with open(HTML_FILE_NAME, "r") as html_fp:
         html_lines = html_fp.readlines()
     return html_lines
